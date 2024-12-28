86526a11e19f32b5ea6c14ba522b4df6f8d1125e
sachin <sk800968@gmail.com>
2024-12-28 13:17:40 +0530

    added routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import CaptainLogin from './pages/CaptainLogin'
+import CaptainSignup from './pages/CaptainSignup'
+
 
 const App = () => {
   return (
-    <div className='bg-red-200'>App</div>
+
+
+   <Routes>
+
+    <Route path='/'  element={<Home/>}/>
+    <Route path='/login'  element={<UserLogin/>}/>
+    <Route path='/signup'  element={<UserSignup/>}/>
+    <Route path='/captain-login'  element={<CaptainLogin/>}/>
+    <Route path='/captain-signup'  element={<CaptainSignup/>}/>
+   </Routes>
   )
 }
 
