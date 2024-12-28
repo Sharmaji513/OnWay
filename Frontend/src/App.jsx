7e7094e160fd084dfb77d3cc4a285c3137e08654
sachin <sk800968@gmail.com>
2024-12-28 17:36:21 +0530

    added start page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
+
 import UserLogin from './pages/UserLogin'
 import UserSignup from './pages/UserSignup'
 import CaptainLogin from './pages/CaptainLogin'
 import CaptainSignup from './pages/CaptainSignup'
+import Start from './pages/Start'
+import Home from './pages/Home'
+
 
 
 const App = () => {
@@ -14,6 +17,7 @@ const App = () => {
    <Routes>
 
     <Route path='/'  element={<Home/>}/>
+    <Route path='/start'  element={<Start/>}/>
     <Route path='/login'  element={<UserLogin/>}/>
     <Route path='/signup'  element={<UserSignup/>}/>
     <Route path='/captain-login'  element={<CaptainLogin/>}/>
---
+++ Frontend/src/pages/Start.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+const Start = () => {
+  return (
+    <div className="bg-zinc-200">
+    <div className="h-screen w-full bg-cover bg-center md:rounded-md  flex justify-between flex-col md:max-w-screen-sm lg:max-w-screen-lg  mx-auto  bg-[url(https://i.pinimg.com/originals/fb/14/3b/fb143b1e6aba0cd635591f661702fca4.gif)]">
+      {/* Header */}
+      <h1 className="text-3xl ml-4 md:ml-8 text-white font-bold mt-2">🤙myCab</h1>
+
+      {/* Content */}
+      <div className="bg-white rounded-lg  shadow-lg py-4 px-4 md:py-4 md:px-5 w-full md:w-[90%] md:my-2  mx-auto ">
+        <h2 className="text-2xl md:text-[30px] font-semibold text-gray-800">
+          Get Started with myCab
+        </h2>
+        <Link to="/login" className="flex items-center justify-center w-full bg-black text-white py-3 rounded-lg mt-5 hover:bg-gray-800 transition duration-300">
+          Continue
+        </Link>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Start;
